3fba42410e297766c084d358f1fd8b5bc8517245
user9000cr <80987734+user9000cr@users.noreply.github.com>
2023-02-19 15:08:09 -0600

    Update Proj_1.m

--- Proj_1.m
+++ Proj_1.m
@@ -1,3 +1,4 @@
+close all
 Ts = 1; % Duracion del simbolo
 L = 16; % Numero de muestras por simbolo
 
@@ -14,14 +15,14 @@ pt_0_75 = rcosdesign(a_0_75,6,L,'normal');
 pt_1 = rcosdesign(a_1,6,L,'normal');
 
 figure(1)
-plot (t,pt_0,'DisplayName','0')
+plot (t,pt_0,'DisplayName','\alpha = 0')
 grid on
 hold on
-
-plot (t,pt_0_25,'DisplayName','0.25')
-plot (t,pt_0_75,'DisplayName','0.75')
-plot (t,pt_1,'DisplayName','1')
+plot (t,pt_0_25,'DisplayName','\alpha = 0.25')
+plot (t,pt_0_75,'DisplayName','\alpha = 0.75')
+plot (t,pt_1,'DisplayName','\alpha = 1')
+xlabel('Ts');
+xticks(-3:Ts:3)
+xticklabels({'-3Ts','-2Ts','-Ts','0','Ts','2Ts','3Ts'});
+legend('\alpha = 0', '\alpha = 0.25', '\alpha = 0.75',  '\alpha = 1')
 hold off
-
-
-
