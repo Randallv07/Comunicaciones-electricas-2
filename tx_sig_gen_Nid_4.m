23101338222024acb57f613d9ce7e7d3c57b055d
Esteban <estearia20@gmail.com>
2023-02-20 16:34:40 -0600

    Se agrega la parte del ruido

--- tx_sig_gen_Nid_4.m
+++ tx_sig_gen_Nid_4.m
@@ -8,7 +8,7 @@ t_step = Ts/L; % Tamaño del paso para muestreo, es correcto ya que divide el ti
                      %dando el tiempo entre muestras
 
 %%%%%%%%%<1. Generacion de onda del pulso > %%%%%%%%%%%%%%%%%%%%%%
-pt = rcosdesign(0.5,6,L,'normal');                             % Genera los puntos del coseno alzado con factor de rodamiento de 0.25, 6 tiempos de símbolo
+pt = rcosdesign(1,6,L,'sqrt');                             % Genera los puntos del coseno alzado con factor de rodamiento de 0.25, 6 tiempos de símbolo
                                                                               % y 100 muestras por símbolo
 pt = pt/(max(abs(pt))); %rescaling to match rcosine, está correcto ya que divide todos los puntos por el máximo valor del coseno alzado
 
@@ -17,8 +17,8 @@ Ns = 1389;                                       % Cantidad de bits
 data_bit = (rand(1,Ns)>0.5);              % Está correcto ya que se genera un vector de dimensión Ns y convierte a valores booleanos
 
 %%%%%%%%%<3. Unipolar a Bipolar (modulacion de amplitud)>%%%%%%%%%%%%%%
-%amp_modulated = 2*ceil(rand(1, Ns)*4) - 5; % Para M = 4
-amp_modulated = 2*data_bit-1;       % Para M = 2
+amp_modulated = 2*ceil(rand(1, Ns)*4) - 5; % Para M = 4
+%amp_modulated = 2*data_bit-1;       % Para M = 2
 
 %%%%%%%%%<4.  Modulacion de pulsos >%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 impulse_modulated = [];
@@ -33,8 +33,9 @@ tx_signal = conv(impulse_modulated, pt);        % Convoluciona la señal modulad
                                                                     % como resultado la señal teóricamente sin ISI
 
 %-------------------Se agrega para el matched filter-----------------------
-matched_out = conv(tx_signal, pt)/100;
-                                                                    
+%matched_out = conv(tx_signal, pt)/100;
+rx_signal = tx_signal + 0.15 * randn ( 1 , length(tx_signal)); 
+
 figure(100)
 subplot (2,1,1)
 stem( t_step: t_step: (Ns*Ts), impulse_modulated, '.');
@@ -49,9 +50,9 @@ title ('pulse shaped')
 
 figure(200)
 for k=3:floor(Ns/2)-1 %k representa la k-ésima muestra
-    tmp = matched_out(((k-1)*2*L+1): (k*2*L));
+    tmp = rx_signal(((k-1)*2*L+1): (k*2*L));
     plot(t_step*(0:(2*L-1)), tmp);
-    axis([0 2 min(matched_out) max(matched_out)]);
+    axis([0 2 min(rx_signal) max(rx_signal)]);
     grid on; hold on
     %pause
 end
