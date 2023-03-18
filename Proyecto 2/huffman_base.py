6ffcca0000c1fce198b95c2b7e3d38a43bcf5b8d
user9000cr <dherrerac00@gmail.com>
2023-03-18 15:11:18 -0600

    Codigo punto 2.4.1

--- Proyecto 2/huffman_base.py
+++ Proyecto 2/huffman_base.py
@@ -220,5 +220,42 @@ print("El tamaño original en bytes es        :", original_size)
 print("El tamaño de la versión comprimida es :", compress_size)
 print("La tasa de compresión es              :", tasa)
 
+#---------------------------------------------------------
+#-                      DESCOMPRESION                    -
+#---------------------------------------------------------
 
+
+##############################
+# Codigo para el punto 2.4.1 #
+##############################
+
+csvfile = open(ruta_diccionario, 'r')
+reader = csv.reader(csvfile)
+bits_a_leer = None
+diccionario = dict()
+
+for row in reader:
+    if(bits_a_leer==None):
+        bits_a_leer = int(row[0])
+    else:
+        diccionario.update({int(row[0]): row[1]})
+
+Decoding = NodeTree(None,None)
+for entrada in diccionario:
+    insert_in_tree(Decoding, diccionario[entrada],entrada)
+
+nodo = Decoding
+data_estimated = []
+for i in range(compressed_length_bit):
+    (l,r) = nodo.children()
+    #print([i,binary_string[i]])
+    if(binary_string[i]=='1'):
+        nodo = r
+    else:
+        nodo = 1
+
+    if type(nodo) is int:
+        data_estimated.append(nodo)
+        #print([i, nodo])
+        nodo = Decoding
 
